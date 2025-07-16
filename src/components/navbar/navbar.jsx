80e66672b90318b89086dc9b6f1096d488cf67d7
Huynh Xuan Son <son500803@gmail.com>
2025-07-16 23:05:36 +0700

    Refactor Navbar component structure and update styling class names

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,28 +1,38 @@
-import "./navbar.scss";
+import "./Navbar.scss";
 import Logo from "../../assets/logo.png";
+import { IoMenu } from "react-icons/io5";
+import { IoMdClose } from "react-icons/io";
+import { useState } from "react";
+import { compile } from "sass";
 
 const Navbar = () => {
-    return <header className="navbar">
-        <nav className="navbar_container wrapper">
-            <a href="#" className="navbar_logo">
-                <img src={Logo} alt="logo" />
-            </a>
-
-            <ul>
-                <li><a href="#">product</a></li>
-                <li><a href="#">compnay</a></li>
-                <li><a href="#">pricing</a></li>
-                <li><a href="#">blog</a></li>
-            </ul>
-
-            <div className="navbar__btns">
-                < a href="#">Login</a>
-                <a href="#" className="btn">
-                    Register
+    const [showNav, setShowNav] = useState(false);
+    return (
+        <header className="navbar">
+            <nav className="navbar__container wrapper">
+                <a href="#" className="navbar__logo" onClick={() => setShowNav(false)}>
+                    <img src={Logo} alt="logo" />
                 </a>
-            </div>
-        </nav>
-    </header>
+                <ul className={`${showNav ? "show" : ""}`}>
+                    <li onClick={() => setShowNav(false)}><a href="#">product</a></li>
+                    <li onClick={() => setShowNav(false)}><a href="#">compnay</a></li>
+                    <li onClick={() => setShowNav(false)}><a href="#">pricing</a></li>
+                    <li onClick={() => setShowNav(false)}><a href="#">blog</a></li>
+                </ul>
+
+                <div className="navbar__btns">
+                    < a href="#">login</a>
+                    <a href="#" className="btn">
+                        register
+                    </a>
+                </div>
+
+                <div className="navbar__menu" onClick={() => setShowNav(!showNav)}>
+                    {showNav ? <IoMdClose /> : <IoMenu />}
+                </div>
+            </nav>
+        </header>
+    )
 };
 
 export default Navbar;
